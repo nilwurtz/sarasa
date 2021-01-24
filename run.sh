06e6b37db5d15c4f4b2587b682b6d4c8e8add628
ragnar1904 <apollo.oth@gmail.com>
2021-01-24 15:33:41 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+current=`dirname $0`
+cd `dirname $0`
+
+if [ ! -d $current/dist ]; then
+    mkdir $current/dist
+fi
+
+if [ ! -f $current/dist/main.js ]; then
+    npm run build
+fi
+
+node dist/main.js $@
