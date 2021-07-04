561da3b7422cf491c9aabea5219415d2d8822d0f
ragnar1904 <apollo.oth@gmail.com>
2021-07-04 19:26:43 +0900

    remove clipboard

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
 import { FilePath } from './domain/FilePath';
-import { ClipBoardDriver } from './driver/ClipBoardDriver';
 import { FileDriver } from './driver/FileDriver';
-import { ClipBoardGateway } from './gateway/ClipBoardGateway';
 import { FileGateway } from './gateway/FileGateway';
-import { ClipBoardUsecase } from './usecase/ClipBoardUsecase';
 import { FileUsecase } from './usecase/FileUsecase';
-import { ImageUsecase } from './usecase/ImageUsecase';
 import { PathUsecase } from './usecase/PathUsecase';
 
-const clipBoardResource = new ClipBoardUsecase(
-  new ClipBoardGateway(new ClipBoardDriver())
-);
-const imageResource = new ImageUsecase();
 const pathResource = new PathUsecase();
 const fileResource = new FileUsecase(new FileGateway(new FileDriver()));
 
